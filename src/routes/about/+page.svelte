d75101411a72eabcab5e2b4eba6782327dda4169
shige99 <shige9923062@gmail.com>
2024-06-09 23:15:45 +0900

    add dir on gitignore

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,59 +4,59 @@
 </svelte:head>
 
 <div class="mb-12">
-    <h2 class="text-2xl font-bold mb-2">基本情報</h2>
-    <p class="text-lg">1999.1.14 沖縄県出身 25歳</p>
-    <p class="text-lg">ソフトウェアエンジニア</p>
-    <p class="text-lg">趣味：野球観戦（巨人ファン）、旅行、妻と外出</p>
-  </div>
+  <h2 class="text-2xl font-bold mb-2">基本情報</h2>
+  <p class="text-lg">1999.1.14 沖縄県出身 25歳</p>
+  <p class="text-lg">ソフトウェアエンジニア</p>
+  <p class="text-lg">趣味：野球観戦（巨人ファン）、旅行、妻と外出</p>
+</div>
   
-  <main class="flex-grow">
-    <section class="mb-12">
-      <h2 class="text-2xl font-bold mb-2">経歴</h2>
-      <ul>
-        <li><span class="font-bold">2022年3月:</span> 琉球大学工学部機械工学コース卒業</li>
-        <li><span class="font-bold">2022年4月:</span> セメントメーカー 工場勤務〜</li>
-        <li><span class="font-bold">2023年5月:</span> 都内SES企業〜</li>
-      </ul>
-    </section>
+<main class="flex-grow">
+  <section class="mb-12">
+    <h2 class="text-2xl font-bold mb-2">経歴</h2>
+    <ul>
+      <li><span class="font-bold">2022年3月:</span> 琉球大学工学部機械工学コース卒業</li>
+      <li><span class="font-bold">2022年4月:</span> セメントメーカー 工場勤務〜</li>
+      <li><span class="font-bold">2023年5月:</span> 都内SES企業〜</li>
+    </ul>
+  </section>
   
-    <section class="mb-12">
-      <h2 class="text-2xl font-bold mb-2">SNS</h2>
-      <div class="overflow-x-auto">
-        <table>
-          <tbody>
-            <tr>
-              <td class="border px-4 py-2">Twitter</td>
-              <td class="border px-4 py-2"
-                ><a
-                  class="text-blue-600 hover:text-blue-500"
-                  href="https://twitter.com/shige99__"
-                  target="_blank">https://twitter.com/shige99__</a
-                ></td
-              >
-            </tr>
-            <tr>
-              <td class="border px-4 py-2">Zenn</td>
-              <td class="border px-4 py-2"
-                ><a
-                  class="text-blue-600 hover:text-blue-500"
-                  href="https://zenn.dev/shige99"
-                  target="_blank">https://zenn.dev/shige99</a
-                ></td
-              >
-            </tr>
-            <tr>
-              <td class="border px-4 py-2">GitHub</td>
-              <td class="border px-4 py-2"
-                ><a
-                  class="text-blue-600 hover:text-blue-500"
-                  href="https://github.com/shige-99"
-                  target="_blank">https://github.com/shige-99</a
-                ></td
-              >
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </section>
-  </main>
+  <section class="mb-12">
+    <h2 class="text-2xl font-bold mb-2">SNS</h2>
+    <div class="overflow-x-auto">
+      <table>
+        <tbody>
+          <tr>
+            <td class="border px-4 py-2">Twitter</td>
+            <td class="border px-4 py-2"
+            ><a
+              class="text-blue-600 hover:text-blue-500"
+              href="https://twitter.com/shige99__"
+              target="_blank">https://twitter.com/shige99__</a
+            ></td
+            >
+          </tr>
+          <tr>
+            <td class="border px-4 py-2">Zenn</td>
+            <td class="border px-4 py-2"
+            ><a
+              class="text-blue-600 hover:text-blue-500"
+              href="https://zenn.dev/shige99"
+              target="_blank">https://zenn.dev/shige99</a
+            ></td
+            >
+          </tr>
+          <tr>
+            <td class="border px-4 py-2">GitHub</td>
+            <td class="border px-4 py-2"
+            ><a
+              class="text-blue-600 hover:text-blue-500"
+              href="https://github.com/shige-99"
+              target="_blank">https://github.com/shige-99</a
+            ></td
+            >
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
+</main>
