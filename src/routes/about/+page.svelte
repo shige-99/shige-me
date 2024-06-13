13032b30b92a8029c71b813d95da897b4946465f
shige99 <shige992306@gmail.com>
2024-06-14 00:20:27 +0900

    update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,7 +5,7 @@
   
 <main class="flex-grow">
   <section class="mb-12">
-    <h2 class="text-2xl font-bold mb-2">Bio</h2>
+    <h2 class="text-2xl font-bold mb-2 underline decoration-neutral-600 underline-offset-4">Bio</h2>
     <ul>
       <li><span class="font-bold">1999</span>&nbsp;Born in Okinawa,Japan</li>
       <li><span class="font-bold">2022</span>&nbsp;Completed the Bachelor's Program<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -17,56 +17,25 @@
   </section>
 
   <section class="mb-12">
-    <h2 class="text-2xl font-bold mb-2">I ♡</h2>
-    <p>Watching Baseball Game, Travel,Life with wife</p>
+    <h2 class="text-2xl font-bold mb-2 underline decoration-neutral-600 underline-offset-4">I ♡</h2>
+    <p>Watching Baseball Game, Travel, Life with wife</p>
   </section>
   
   <section class="mb-12">
-    <h2 class="text-2xl font-bold mb-2">On the web</h2>
-    <div class="overflow-x-auto">
-      <table class="w-full">
-        <tbody>
-          <tr>
-            <td class="px-4 py-2">Twitter</td>
-            <td class="px-4 py-2"
-            ><a
-              class="text-blue-600 hover:text-blue-500"
-              href="https://twitter.com/shige99__"
-              target="_blank">https://twitter.com/shige99__</a
-            ></td
-            >
-          </tr>
-          <tr>
-            <td class="px-4 py-2">Zenn</td>
-            <td class="px-4 py-2"
-            ><a
-              class="text-blue-600 hover:text-blue-500"
-              href="https://zenn.dev/shige99"
-              target="_blank">https://zenn.dev/shige99</a
-            ></td
-            >
-          </tr>
-          <tr>
-            <td class="px-4 py-2">GitHub</td>
-            <td class="px-4 py-2"
-            ><a
-              class="text-blue-600 hover:text-blue-500"
-              href="https://github.com/shige-99"
-              target="_blank">https://github.com/shige-99</a
-            ></td
-            >
-          <tr>
-            <td class="px-4 py-2">しずかなインターネット</td>
-            <td class="px-4 py-2"
-            ><a
-              class="text-blue-600 hover:text-blue-500"
-              href="https://sizu.me/shige99"
-              target="_blank">https://sizu.me/shige99</a
-            ></td
-            >
-          </tr>
-        </tbody>
-      </table>
+    <h2 class="text-2xl font-bold mb-4 underline decoration-neutral-600 underline-offset-4">On the web</h2>
+    <div class="flex space-x-4">
+      <a href="https://twitter.com/shige99__">
+        <img src="/images/x-logo.png" alt="x-logo" class="h-10 w-10 ">
+      </a>
+      <a href="https://zenn.dev/shige99">
+        <img src="/images/zenn-logo.png" alt="zenn-logo" class="h-10 w-10 ">
+      </a>
+      <a href="https://github.com/shige-99">
+        <img src="/images/github-logo.png" alt="github-logo" class="h-10 w-10 ">
+      </a>
+      <a href="https://sizu.me/shige99">
+        <img src="/images/sizu-logo.png" alt="sizu-logo" class="h-10 w-10 ">
+      </a>
     </div>
   </section>
 </main>
