57552900ee4beffdb571e3cfa22137b51fd3b657
shige99 <shige992306@gmail.com>
2024-06-15 22:57:27 +0900

    lit bit change

--- src/lib/components/ArticleLink.svelte
+++ src/lib/components/ArticleLink.svelte
@@ -6,7 +6,7 @@
 <a 
     rel="prefetch"
     href="/articles/{article.slug}"
-    class="m-4 p-4 max-w-sm mx-auto bg-primary-light rounded-lg border border-gray-200 flex items-center space-x-4 transition-colors duration-200 ease-in-out hover:bg-primary-dark"
+    class="m-4 p-4 max-w-sm mx-auto bg-primary-light rounded-lg border border-primary-font flex items-center space-x-4 transition-colors duration-200 ease-in-out hover:bg-primary-dark"
 >
     <div>
         <div class="text-xl font-medium text-primary-font mb-3">{article.metadata.title}</div>
