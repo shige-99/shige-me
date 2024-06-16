905aded0be7cc14e2e564ea7cabf07b691a9162e
shige99 <shige992306@gmail.com>
2024-06-16 17:10:56 +0900

    add button hover style

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -24,19 +24,19 @@
   <section class="mb-12">
     <h2 class="text-2xl font-bold mb-4 underline decoration-primary-light underline-offset-4">On the web</h2>
     <div class="flex flex-col space-y-2">
-      <a href="https://github.com/shige-99" class="inline-flex items-center text-primary bg-primary-button rounded-lg focus:ring-4 focus:outline-none px-2 py-1 w-32">
+      <a href="https://github.com/shige-99" class="inline-flex items-center text-primary bg-primary-button rounded-lg focus:ring-4 focus:outline-none px-2 py-1 w-32 hover:bg-primary-font">
         <img src="/images/github-logo.png" alt="github-logo" class="h-4 w-4 mr-2">
         <span>@shige-99</span>
       </a>
-      <a href="https://twitter.com/shige99__" class="inline-flex items-center text-primary bg-primary-button rounded-lg focus:ring-4 focus:outline-none px-2 py-1 w-32">
+      <a href="https://twitter.com/shige99__" class="inline-flex items-center text-primary bg-primary-button rounded-lg focus:ring-4 focus:outline-none px-2 py-1 w-32 hover:bg-primary-font">
         <img src="/images/x-logo.png" alt="x-logo" class="h-4 w-4 mr-2">
         <span>@shige99__</span>
       </a>
-      <a href="https://zenn.dev/shige99" class="inline-flex items-center text-primary bg-primary-button rounded-lg focus:ring-4 focus:outline-none px-2 py-1 w-32">
+      <a href="https://zenn.dev/shige99" class="inline-flex items-center text-primary bg-primary-button rounded-lg focus:ring-4 focus:outline-none px-2 py-1 w-32 hover:bg-primary-font">
         <img src="/images/zenn-logo.png" alt="zenn-logo" class="h-4 w-4 mr-2">
         <span>@shige99</span>
       </a>
-      <a href="https://sizu.me/shige99" class="inline-flex items-center text-primary bg-primary-button rounded-lg focus:ring-4 focus:outline-none px-2 py-1 w-32">
+      <a href="https://sizu.me/shige99" class="inline-flex items-center text-primary bg-primary-button rounded-lg focus:ring-4 focus:outline-none px-2 py-1 w-32 hover:bg-primary-font">
         <img src="/images/sizu-logo.png" alt="sizu-logo" class="h-4 w-4 mr-2">
         <span>@shige99</span>
       </a>
