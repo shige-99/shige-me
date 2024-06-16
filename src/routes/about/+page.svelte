b34be2afcfbc78ce639070a32b2a393606046a43
shige99 <shige992306@gmail.com>
2024-06-16 16:39:20 +0900

    update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,13 +3,13 @@
   <meta name="description" content="About me" />
 </svelte:head>
   
-<main class="flex-grow container">
+<main class="flex-grow container min-h-screen">
   <section class="mb-12">
     <h2 class="text-2xl font-bold mb-2 underline decoration-primary-light underline-offset-4">Bio</h2>
-    <ul class="text-primary-font">
+    <ul class="text-primary-font leading-7">
       <li><span class="font-bold">1999</span>&nbsp;Born in Okinawa,Japan</li>
-      <li><span class="font-bold">2022</span>&nbsp;Completed the Bachelor's Program<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                             in the Graduate Shool of Mechanical<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <li><span class="font-bold">2022</span>&nbsp;Completed the Bachelor's Program<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                             in the Graduate Shool of Mechanical<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                                              Engineering at University of Ryukyus</li>
       <li><span class="font-bold">2022</span>&nbsp;Worked at cement manufacturer</li>
       <li><span class="font-bold">2023</span>&nbsp;Working at SES Co.</li>
@@ -18,7 +18,7 @@
 
   <section class="mb-12">
     <h2 class="text-2xl font-bold mb-2 underline decoration-primary-light underline-offset-4">I ♡</h2>
-    <p>Watching Baseball Game, Travel, Life with wife</p>
+    <p class="leading-7">Watching Baseball Game, Travel, Life with wife</p>
   </section>
   
   <section class="mb-12">
